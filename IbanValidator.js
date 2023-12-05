d7e688635ad461a8b4c71033415f4d8620595352
Mark Jeremy Essa <alexanderessa@hotmail.com>
2023-12-05 17:13:37 +0100

    update according to heyflow

--- IbanValidator.js
+++ IbanValidator.js
@@ -1,12 +1,3 @@
-const debounce = (func, timeout = 250) => {
-  let timer;
-  return (...args) => {
-    const boundFunc = func.bind(this, ...args);
-    clearTimeout(timer);
-    timer = setTimeout(boundFunc, timeout);
-  };
-};
-
 const validateIban = async (inputData) => {
   try {
     const response = await fetch("https://eo710ouhxzmvxt2.m.pipedream.net", {
@@ -24,34 +15,18 @@ const validateIban = async (inputData) => {
   }
 };
 
-/* 
-  if we are able to select only the Heyflow IBAN input field and it has an event of heyflow-change
-  
-  window.addEventListener("load", () => {
-    document.querySelector("input[name='IBAN']").addEventListener("heyflow-change", debounce(async (event) => {
-      const { isValid } = await validateIban(event.detail.fieldsSimple["IBAN"]);
+window.addEventListener("heyflow-change", async (event) => {
+  console.log(event);
+  if (
+    !event.detail.fieldsSimple["IBAN"] &&
+    event.detail.fieldsSimple["IBAN"] !== ""
+  ) {
+    return;
+  }
 
-      if (!isValid) {
-        window.alert("Invalid IBAN");
-      }
-    }));
-  });
+  const { isValid } = await validateIban(event.detail.fieldsSimple["IBAN"]);
 
-
-*/
-window.addEventListener(
-  "heyflow-change",
-  // assuming that heyflow-change follows the behavior of input change
-  debounce(async (event) => {
-    if (!event.detail.fieldsSimple["IBAN"]) {
-      return;
-    }
-
-    //assuming that the heyflow label input name is iban
-    const { isValid } = await validateIban(event.detail.fieldsSimple["IBAN"]);
-
-    if (!isValid) {
-      window.alert("Invalid IBAN");
-    }
-  })
-);
+  if (!isValid) {
+    window.alert("Invalid IBAN");
+  }
+});
